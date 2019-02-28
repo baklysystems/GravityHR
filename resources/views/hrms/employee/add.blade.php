6c5e0812eba7538fecea497a36f73423f5908cbe
Justin McHood <suitedconnectorbpo@Suited-08.local>
2019-03-01 05:43:11 +0800

    test

--- resources/views/hrms/employee/add.blade.php
+++ resources/views/hrms/employee/add.blade.php
@@ -11,7 +11,7 @@
 				</li>
 				{{--
 				<li class="breadcrumb-active">
-					<a href="#"> Edit Detailss</a>
+					<a href="#"> Edit Details</a>
 				</li>--}}
 				<li class="breadcrumb-link">
 					<a href="/dashboard"> Employees </a>
